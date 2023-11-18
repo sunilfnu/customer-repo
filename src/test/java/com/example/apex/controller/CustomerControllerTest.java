ce5ec45042543d923e4d040d68f2632a431c98b9
= <=>
2023-11-17 22:34:41 -0500

    Implement builder class

--- src/test/java/com/example/apex/controller/CustomerControllerTest.java
+++ src/test/java/com/example/apex/controller/CustomerControllerTest.java
@@ -12,6 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,132 +22,77 @@ import static org.mockito.Mockito.when;
 
 class CustomerControllerTest {
     private Logger logger = LoggerFactory.getLogger(CustomerControllerTest.class);
-
-    /*
-     * Roshan Change
-     * */
-
-    /**
-     *  Method Signature
-     *  1) When invoking that method, what arguments to pass
-     *  2) What is the return type
-     *  3) Visibility of that method
-     * */
-
-
-    /**
-     * 3As principle
-     * Arrange
-     * Act
-     * Assert
-     */
-
     @Mock
     CustomerRepository repo;
     @InjectMocks
     CustomerController controller;
 
     @BeforeEach
-    void setup() {
-        MockitoAnnotations.openMocks(this);
+    void setup(){
+        MockitoAnnotations.openMocks(this);  // mockito initializes mocks before the execution of each individual test method.
     }
+    @Test // how to test it orderly?
 
-    @Test
-    void testCreateCustomer() {
-        logger.info("Inside test create...");
-        // Arrange
-        //Creating Customer
-        //adfasdf
-        Customer customer = new Customer();
-        customer.setFirstName("Thoth");
+    public void testCreateCustomer(){
 
-        Customer savedCustomer = new Customer();
-        savedCustomer.setId(1001L);
-        savedCustomer.setFirstName("Thoth");
-
-        logger.info("After customer is created...");
-
-        when(repo.save(any())).thenReturn(savedCustomer);
-
-        // Act
+        Customer customer = Customer.builder().id(10L).firstName("nish").lastName("ghi").emailId("n@gmail.com").build();
+        Customer expcustomer = Customer.builder().id(10L).firstName("nish").lastName("ghi").emailId("n@gmail.com").build();
+        when(repo.save(any())).thenReturn(expcustomer);
+        //Act
         Customer actual = controller.createCustomer(customer);
-
-        // Assert
-        assertEquals(actual.getId(), savedCustomer.getId());
-        assertEquals(actual.getFirstName(), savedCustomer.getFirstName());
-    }
-
-
-    //test2
-
-    @Test
-    void testFindById() {
-        logger.info("Inside test find by Id");
-        long id = 1001l;
-
-        Customer returnedCustomer = new Customer();
-        returnedCustomer.setId(1001L);
-        returnedCustomer.setFirstName("Thoth");
-
-        when(repo.findById(any())).thenReturn(Optional.of(returnedCustomer));
-
-        // Act
-        Customer customer = controller.getById(id);
-
         //Assert
-        assertEquals(id, customer.getId());
+        assertEquals(expcustomer.getId(),actual.getId());
+        assertEquals(expcustomer.getFirstName(),actual.getFirstName());
+        assertEquals(expcustomer.getLastName(),actual.getLastName());
     }
 
-    //test3
-
-
     @Test
-    void testGetAllCustomer() {
-        Customer customer1 = new Customer();
-        customer1.setId(1L);
-        customer1.setFirstName("Andrew");
+    public void testGetAllCustomer() {
+        Customer customer1 = Customer.builder().id(1L).firstName("nis").build();
+        Customer customer2 = Customer.builder().id(2L).firstName("nik").build();
 
-        Customer customer2 = new Customer();
-        customer2.setId(2L);
-        customer2.setFirstName("Austin");
-
-        List<Customer> customerList = new ArrayList<>();
-        customerList.add(customer1);
-        customerList.add(customer2);
-
-        Mockito.when(repo.findAll()).thenReturn(customerList);
-
-        List<Customer> customer = controller.getAll();
-
-        assertEquals(2, customer.size());
-        assertEquals(customer, customerList);
+        List<Customer> customerList = Arrays.asList(customer1, customer2);
+        when(repo.findAll()).thenReturn(customerList);
+        List<Customer> customers = controller.getAll();
+        assertEquals(2, customers.size());
+        assertEquals(customerList, customers);
         Mockito.verify(repo, Mockito.times(1)).findAll();
     }
-
-
-    //Pushed the code so that easier to pull rather than see my screen
-    //I am confused on this method. Specifically, on how to test this method, because it has no return type, so
-    // return null rakhne ki k rakhne also Mockito.when garda k rakhne
     @Test
-    public void testDeleteCustomerById() {
+    public void testDeleteCustomerById(){
         long id = 120L;
-        Customer customer = new Customer();
-        customer.setId(id);
-        customer.setFirstName("Nishchal");
+        Customer customer = Customer.builder().id(120L).firstName("niki").lastName("priya").build();
+        Customer expecCustomer = Customer.builder().id(120L).firstName("niki").lastName("priya").build();
         controller.deleteById(id);
-
         Mockito.verify(repo, Mockito.times(1)).deleteById(Mockito.anyLong());
     }
 
     @Test
-    public void testById() {  // this code is not working as expected,so I would like to ask sunil to explain.
-        Customer expectedCustomer = new Customer();
-        long id = 1L;
-        expectedCustomer.setId(id);
-        expectedCustomer.setFirstName("Bibek");
-        Customer actualCustomer = controller.getById(id);
-        // Mockito.verify(repo, Mockito.times(1)).findById(Mockito.anyLong());
-        Mockito.verify(repo, Mockito.times(1)).findById(id);
-        assertEquals(expectedCustomer, actualCustomer);
+    public void testFindById(){
+        Customer customer = Customer.builder().id(12L).firstName("nis").lastName("ghi").build();
+        Customer  expectedCustomer = Customer.builder().id(12L).firstName("nis").lastName("ghi").build();
+        logger.info("find byid is executed");
+        when(repo.findById(12L)).thenReturn(Optional.of(expectedCustomer));
+        Customer actualCust = controller.getById(12L);
+        assertEquals(expectedCustomer.getId(),actualCust.getId());
+
     }
+    @Test
+    public void testUpdateCustomer(){
+        // we  give this below value
+        Customer customer = Customer.builder().id(2L).firstName("nishchal").lastName("Ghimire").build();
+        // again test against the give above value.
+        Customer expectedCustomer = Customer.builder().id(2L).firstName("nishchal").lastName("Ghimire").build();
+        logger.info("After the customer is updated");
+
+        when(repo.save(any(Customer.class))).thenReturn(expectedCustomer);
+        //Act
+        Customer actual = controller.updateCustomer(customer);
+        //Assert
+        assertEquals(expectedCustomer.getId(),actual.getId());
+        assertEquals(expectedCustomer.getFirstName(),actual.getFirstName());
+        assertEquals(expectedCustomer.getLastName(),actual.getLastName());
+
+    }
+
 }
